5d3db73ee9b58a98fd7c01a7429723b259565e49
jayzeng <jay.zeng@baifu-tech.net>
2025-05-06 11:25:51 +0800

    add

--- api/routes/web.php
+++ api/routes/web.php
@@ -4,4 +4,5 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 
 Route::get('/posts/latest-report', [PostController::class, 'latestReport']);
+Route::get('/', [PostController::class, 'index']);
 Route::apiResource('posts', PostController::class);
